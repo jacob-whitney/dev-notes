149b4c025aa41519b634072f4f5a4b23f694ad5f
Jacob Whitney (OCLS Desktop) <jwhitney@ocls.info>
2025-06-30 14:37:11 -0400

    Update linux/processes/setup-java.sh

--- linux/processes/setup-java.sh
+++ linux/processes/setup-java.sh
@@ -8,12 +8,13 @@ echo "this system has the latest info on available packages. Continue?"
 
 sudo apt update &&
 
-echo "Checking if java is already installed"
+echo "Checking if Java is already installed"
 
 java --version
 
 # if not installed
-echo "Installing JDK. Which version do you want to install?"
+echo "Installing Java Runtime Environment (JRE), part of the open JDK."
+echo "Which version do you want to install?"
 # take user input for JDK version number
 
 sudo apt install openjdk-<ver>-jre-headless &&
@@ -21,16 +22,22 @@ sudo apt install openjdk-<ver>-jre-headless &&
 # if downloading specific version doesn't work
 sudo apt install default-jre &&
 
-echo "Confirming JDK version installed"
+echo "Confirming Java version installed"
 
 java --version
 
-echo "Additional resources:"
-echo " - https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-on-ubuntu-22-04"
-
-echo "Checking if JDK is also installed"
+echo "Checking that JDK is installed for compiling and running Java-based software."
 
 javac -version
 
 # if not installed
+echo "Installing JDK."
+
 sudo apt install default-jdk
+
+echo "Confirming JDK version installed
+
+javac -version
+
+echo "Additional resources:"
+echo " - https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-on-ubuntu-22-04"
